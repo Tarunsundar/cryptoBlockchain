d173a2b3970274448ab8bb65739ddbfddabb59a6
Tarunsundar <47869186+Tarunsundar@users.noreply.github.com>
2022-07-07 16:25:40 +0100

    Update SimpleBlockChain.sol

--- SimpleBlockChain.sol
+++ SimpleBlockChain.sol
@@ -2,60 +2,59 @@
 pragma solidity ^0.8.0;
 
 contract Transaction{
-	struct Details {
-		int AccountNumber;
-		int AccountBalance;
-		string AccountName;
-	}
+   struct Details 
+   {
+	int AccountNumber;
+	int AccountBalance;
+	string AccountName;
+   }
 
-	struct tDetails {
-		Details senderAcc;
-		Details recieverAcc;
-		int tAmount;
-		address addr;
-	}
+   struct tDetails 
+   {
+	Details senderAcc;
+	Details recieverAcc;
+	int tAmount;
+	address addr;
+   }
 
-	struct oDetails {
-		bytes32 transactionDetails;
-		bytes32 currentHash;
-		bytes32 previousHash;
-	}
+   struct oDetails {
+	bytes32 transactionDetails;
+	bytes32 currentHash;
+	bytes32 previousHash;
+   }
 
-	oDetails[] blockChain;
+   oDetails[] blockChain;
 	
-	modifier costs(uint _amount) {
-      require(
-         msg.value >= _amount,
-         "Not enough Ether provided."
-      );
-      _;
-      if (msg.value > _amount)
-         msg.sender.transfer(msg.value - _amount);
+   modifier costs(uint _amount) 
+   {
+        require(msg.value >= _amount,"Not enough Ether provided.");
+        if (msg.value > _amount)
+            msg.sender.transfer(msg.value - _amount);
    }
    
    function emptyBlockchain(tDetails transacDetails) public returns oDetails{
      oDetails.transactionDetails = transacDetails;
-	 oDetails.currentHash = sha256(abi.encodePacked(transacDetails));
-	 oDetails.previousHash = 0;  //genisis block
+     oDetails.currentHash = sha256(abi.encodePacked(transacDetails));
+     oDetails.previousHash = 0;  //genisis block
      returns oDetails
    }
  
    function nonEmptyBlockchain(tDetails transacDetails) public returns oDetails{
      oDetails.transactionDetails = transacDetails;
-	 oDetails.currentHash = sha256(abi.encodePacked(transacDetails));
-	 oDetails.previousHash = blockChain[blockChain.length-1];  //previous block
+     oDetails.currentHash = sha256(abi.encodePacked(transacDetails));
+     oDetails.previousHash = blockChain[blockChain.length-1];  //previous block
      returns oDetails;
    }
    
-	function insertTransaction(tDetails transacDetails) public returns oDetails[]{
+   function insertTransaction(tDetails transacDetails) public returns oDetails[]{
 	  (bool success,) = address(oDetails.tDetails.recieverAcc).call(abi.encodeWithSignature("nonExistingFunction()"));
-      require(success);
+          require(success);
 	  costs(transacDetails.tAmount);
 	  if(blockChain.length == 0){
 	    blockChain.push(emptyBlockchain(transacDetails));
 	  } else {
 	    blockChain.push(nonEmptyBlockchain(transacDetails));
-      }
+          }
 	  returns blockChain;
 	}
 }
